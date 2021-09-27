75e9619c9e2dde18d21a6434051be2ac7e808435
Bernice Dainty <bernicedainty498@gmail.com>
2021-09-27 19:37:08 +0530

    Add basic UI layout for application
    Signed-off-by: Bernice Dainty <bernicedainty498@gmail.com>

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+function App() {
+
+    return (
+        <div>
+            <h1>How fast do you type?</h1>
+            <textarea />
+            <h4>Time remaining: XYZ</h4>
+            <button>Start</button>
+            <h1>Word count: 'n'</h1>
+        </div>
+    )
+}
+
+export default App
