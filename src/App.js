96937e6d9c0bf7bdd63c04ce046af26ab56d5881
Bernice Dainty <bernicedainty498@gmail.com>
2021-10-02 10:41:42 +0530

    General | Use State Hook to track text input
    Signed-off-by: Bernice Dainty <bernicedainty498@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from "react"
+import React, {useState} from "react"
 
 function App() {
+
+    const [text, setText] = useState('')
+
+    function handleChange(event) {
+        const {value} = event.target
+        setText(value)
+    }
 
     return (
         <div>
             <h1>How fast do you type?</h1>
-            <textarea />
+            <textarea 
+                onChange = {handleChange}
+                value = {text}
+            />
             <h4>Time remaining: XYZ</h4>
             <button>Start</button>
             <h1>Word count: 'n'</h1>
