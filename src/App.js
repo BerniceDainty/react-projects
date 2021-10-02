9e92ab20e992bf4dd281b0bf3834b1945c263301
Bernice Dainty <bernicedainty498@gmail.com>
2021-10-02 15:47:49 +0530

    Use customHook for the logic
    - Move code to custom hook and use the imported values and methods using
      the custom hook
    
    Signed-off-by: Bernice Dainty <bernicedainty498@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,51 +1,17 @@
-import React, {useState, useEffect, useRef} from "react"
+import React from "react"
+import useWordGame from "./hooks/useWordGame"
 
 function App() {
 
-    const [text, setText] = useState('')
-    const [timeRemaining, setTimeRemaining] = useState(10)
-    const [isTimeRunnning, setIsTimeRunning] = useState(false)
-    const [wordCount, setWordCount] = useState(0)
-    const textboxRef = useRef(null)
-
-    function handleChange(event) {
-        const {value} = event.target
-        setText(value)
-    }
-
-    function calculateWordCount(wordsTyped) {
-        const filteredWords = wordsTyped.trim().split(' ').filter(word => word!=='')
-        return filteredWords.length
-    }
-
-    function startGame() {
-        setTimeRemaining(10)
-        setIsTimeRunning(true)
-        setWordCount(0)
-        setText('')
-        textboxRef.current.disabled = false
-        /**
-         * The above code enables the input box manually because, all the react methods are performed
-         * asynchronously which in our code makes the below line of focusing the textbox useless as the
-         * textbox is not enabled ((i.e) isTimeRunning is still 'false' when the below line executes)
-         */
-        textboxRef.current.focus()
-    }
-
-    function endGame() {
-        setIsTimeRunning(false)
-        setWordCount(calculateWordCount(text))
-    }
-
-    useEffect(()=>{
-        if(isTimeRunnning && timeRemaining > 0) {
-            setTimeout(()=>{
-                setTimeRemaining((timeRemaining) => timeRemaining-1)
-            }, 1000)
-        } else if (timeRemaining === 0) {
-            endGame()
-        }
-    },[timeRemaining, isTimeRunnning])
+    const {
+        textboxRef,
+        handleChange,
+        text,
+        isTimeRunnning,
+        timeRemaining,
+        startGame,
+        wordCount
+    } = useWordGame()
 
     return (
         <div>
