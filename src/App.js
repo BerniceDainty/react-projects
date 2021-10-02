1b9fea4be49e4882151ed86143d210a135406b79
Bernice Dainty <bernicedainty498@gmail.com>
2021-10-02 11:14:02 +0530

    Calculate the words typed by user
    Signed-off-by: Bernice Dainty <bernicedainty498@gmail.com>

--- src/App.js
+++ src/App.js
@@ -9,6 +9,11 @@ function App() {
         setText(value)
     }
 
+    function calculateWordCount(wordsTyped) {
+        const filteredWords = wordsTyped.trim().split(' ').filter(word => word!=='')
+        return filteredWords.length
+    }
+
     return (
         <div>
             <h1>How fast do you type?</h1>
@@ -17,7 +22,7 @@ function App() {
                 value = {text}
             />
             <h4>Time remaining: XYZ</h4>
-            <button>Start</button>
+            <button onClick = {()=> calculateWordCount(text)}>Start</button>
             <h1>Word count: 'n'</h1>
         </div>
     )
